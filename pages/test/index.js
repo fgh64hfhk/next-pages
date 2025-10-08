c5d7d0e7f9acacc49aac82d007cc74184fb9bed9
tony <bear200806@gmail.com>
2025-10-08 11:57:45 +0800

    test for useEffect with request animation frame

--- pages/test/index.js
+++ pages/test/index.js
@@ -15,13 +15,18 @@ export default function Parent() {
   return (
     <div className="w-full h-[100dvh] flex justify-center items-center gap-1 bg-amber-50">
       <h1>Parent Content</h1>
-      <button className="bg-black text-white cursor-pointer" onClick={() => setId((prev) => prev + 1)}>Set ID</button>
-      <Child value={id}/>
+      <button
+        className="bg-black text-white cursor-pointer"
+        onClick={() => setId((prev) => prev + 1)}
+      >
+        Set ID
+      </button>
+      <Child value={id} />
     </div>
   );
 }
 
-function Child({value}) {
+function Child({ value }) {
   useLayoutEffect(() => {
     console.log("Child: useLayoutEffent");
     return () => console.log("Child: useLayoutEffent");
