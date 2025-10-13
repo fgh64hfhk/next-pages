6786e50cb4df23cb6f23349a1ec7bee76ab05980
tony <bear200806@gmail.com>
2025-10-13 11:07:12 +0800

    carousel and qq theme.css

--- pages/carousel_2/index.js
+++ pages/carousel_2/index.js
@@ -1,20 +1,29 @@
 import QQLayout from "@/layouts/QQLayout";
 import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import Image from "next/image";
 
 export default function carousel_2() {
-  const [pageData, setPageData] = useState({
-    firstGame: {},
-    displayGames: [],
-    allGames: [],
-    currentIdx: 0,
-  });
+  // const [pageData, setPageData] = useState({
+  //   firstGame: {},
+  //   displayGames: [],
+  //   allGames: [],
+  //   currentIdx: 0,
+  // });
 
   return (
     <div className="w-full flex flex-col gap-y-4 bg-[#073926]">
       <div className="w-full flex flex-col gap-3">
         <div className="flex items-center justify-between">
-          <h1 className="text-center">Title</h1>
+          <h1 className="text-[24px] font-bold text-white">Title</h1>
+          <div className="flex items-center gap-2">
+            <button className="w-[30px] h-[30px] flex justify-center items-center bg-[#092f22] border-[#18543f] rounded-sm">
+              <ChevronLeft stroke="#ffffff" />
+            </button>
+            <button className="w-[30px] h-[30px] flex justify-center items-center bg-[#092f22] border-[#18543f] rounded-sm">
+              <ChevronRight stroke="#ffffff" />
+            </button>
+          </div>
         </div>
         <div className="w-full flex gap-2.5">
           <div className="w-[30%]">
@@ -29,7 +38,7 @@ export default function carousel_2() {
               />
             </div>
           </div>
-          <div className="w-[70%] flex overflow-y-scroll">
+          <div className="w-[70%] flex overflow-y-scroll hidden-scrollbar">
             {Array.from({ length: 5 }).map((_, idx) => (
               <div key={idx} className="w-full shrink-0 border bg-amber-400">
                 <div className="w-full h-full flex flex-wrap gap-2.5">
